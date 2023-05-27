ef167784d8accce74ab7eed994bef45bf9a2f975
CodingMonkey <reallycodingmonkey@gmail.com>
2023-05-27 22:48:43 +0200

    Chunk 2: API implementation
    API: functional.
    Tables: missing.
    Plots: missing.

--- functions.R
+++ functions.R
@@ -12,7 +12,7 @@ BINANCE.GET <- function(API_root,
   #> wrapper to call API queries from Binance server
   #> Print user feedback if ERROR
   
-  cat(sprintf("Requesting '%s' to %s. Other param: %s\n", API_query, API_root, API_param))
+  cat(sprintf("\nRequest: '%s' [%s] to %s", API_query, API_param, API_root))
   
   #defines query parameters
   totalParams <- API_param #default
@@ -103,3 +103,45 @@ liquidity.tableInterpreter <- function(jsonPath){
   else{
     cat(jsonPath, ": not FOUND\n")}
 }
+
+getPrice <- function(price_df, coin, refCoin="USDT"){
+  #> scan the price_df dataframe for symbol COINUSDT or USDTCOIN respectively.
+  #> Returns price expressed in 'refCoin'
+  
+  errorMessage <-function(ss){
+    message("ERROR in getPrice(), selected coin has more than one symbol. This should not be possible. :'(")
+    print(ss)
+    stop()}
+  
+  if (coin==refCoin){return(1)} # SANITY-CHECK
+  
+  # Possible SYMBOLS to search for
+  symbol_for <- paste0(coin,refCoin)
+  symbol_rev <- paste0(refCoin,coin) #reverse symbol
+  
+  
+  # ITERATION 1: symbol_for
+  ss <- subset(price_df, symbol==symbol_for)
+  if (nrow(ss)>1){errorMessage(ss)} #Quality check: this should be impossible
+  
+  else if (nrow(ss)==1){ 
+    #that's the desired result: symbol should be unique
+    price <- ss[1,"price", drop=T]
+    return(price)}
+  
+  # ITERATION 2: symbol_rev 
+  #> this part is only read if symbol_for was not found
+  rev <- subset(price_df, symbol==symbol_rev)
+  
+  if (nrow(rev)>1){errorMessage(ss)} #error check
+  else if (nrow(ss)==1){ 
+    #that's the desired result: symbol should be unique
+    price_rev <- ss[1,"price", drop=T]
+    price <- 1/price_rev
+    return(price)}
+  else{
+    # USER FEEDBACK
+    message(sprintf("'%s' [%s/%s] price was not found into provided 'price_df'", coin, symbol_for, symbol_rev))
+    return(NA)}
+}
+
