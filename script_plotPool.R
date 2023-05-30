e4adf9902847220d6679d59cd2b19158f4e8bb88
CodingMonkey <reallycodingmonkey@gmail.com>
2023-05-30 18:56:50 +0200

    Notebook preparation complete!
    Consider it an Alpha version. It can be run on it's own, more feature have to be added.

--- script_plotPool.R
+++ script_plotPool.R
@@ -3,17 +3,11 @@ LiqPlots_Trends <- function(){
   # Plots TREND price.
   # All variable come from GlobalEnv
 
-  ## X-RANGE options
-  # calculate xLim (min and max dates)
+  ## DATE RANGE options
+  # calculate xLim
   xLim_left <- start_date %>% as.POSIXct(tz = "UTC")
   xLim_right <- end_date %>% as.POSIXct(tz = "UTC")
   plot_lim <- c(xLim_left, xLim_right)
-  
-  ## Y-RANGE options
-  #> Default is to plot up to +50% ref Price (up) and 1/2 price (down)
-  #> If real data are outseide of this range, then it plots 10% above and below min and max
-  yLim_up <- max(entryPrice*1.5, max(snap_H$PoolPrice)*1.1)
-  yLim_down <- min(entryPrice*0.5, min(snap_H$PoolPrice)*0.9)
   
   # CONVERTS DATES to POSIXct
   snap_H[,"Date_UTC"] %<>% as.POSIXct(tz = "UTC")
@@ -30,7 +24,6 @@ LiqPlots_Trends <- function(){
           panel.grid.minor.y=element_line(linetype="dashed"))+
     ggtitle(poolName)+
     scale_x_datetime(timezone = "UTC", limits = plot_lim) +
-    scale_y_continuous(limits = c(yLim_down, yLim_up))+
     #scale_x_datetime(limits = plot_lim) + #v3.5: removed to allow stop-loss label to be plotted outside of area.
     xlab("Datetime (UTC)")
   
@@ -38,6 +31,7 @@ LiqPlots_Trends <- function(){
   plot1 <- plot0 +
     geom_line(data=snap_H, aes(x=Date_UTC, y=PoolPrice), na.rm = TRUE) +
     geom_point(aes(y=PoolPrice, fill=Label),shape=21, colour= "black",  size=2, na.rm = TRUE) +
+    scale_y_continuous(limits = c(0,max(1.6*entryPrice, 1.2*snap_H$PoolPrice)))+
     ylab(poolName)
   
   ## [Optional] ADD refData
@@ -48,10 +42,10 @@ LiqPlots_Trends <- function(){
   }
   
   ## 2. ADD: annotations (top layer)
-  plot2 <- plot1 +
+  plot1 <- plot1 +
     geom_hline(yintercept = entryPrice, linetype="dashed") +
     annotate("label", x = xLim_left, y=entryPrice*1.1, label=as.character(paste0("Entry Price: ",round(entryPrice,3),"")),
              size=3.5, hjust=0)
 
-  return(plot2)
+  return(plot1)
 }
